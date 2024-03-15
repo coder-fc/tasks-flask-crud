a230f07561aa8cba4c23611b2467927fd04134c4
DevFC-M <felipech3344@gmail.com>
2024-03-14 21:25:04 -0300

    Add app.py & requirements.txt

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+# __name__ = "__main__"
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello world"
+
+
+@app.route("/about")
+def about():
+    return ("Pagina Sobre")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
