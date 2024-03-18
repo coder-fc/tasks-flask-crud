8d807722becb335ea6a8a5991237ed0535f698ea
DevFC-M <felipech3344@gmail.com>
2024-03-18 20:57:14 -0300

    Create and Read API

--- app.py
+++ app.py
@@ -1,17 +1,46 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Taks
 
 # __name__ = "__main__"
 app = Flask(__name__)
 
+# CRUD
+# Create, Read, Update and Delete
+# Tabela: Tarefa
 
-@app.route("/")
-def hello_world():
-    return "Hello world"
+tasks = []
+task_id_control = 1
 
 
-@app.route("/about")
-def about():
-    return ("Pagina Sobre")
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    global task_id_control
+    data = request.get_json()
+    new_task = Taks(id=task_id_control, title=data['title'],
+                    description=data.get("description", ""))
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message": "Nova tarefa criada com sucesso"})
+
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+    output = {
+        "tasks": task_list,
+        "total_tasks": len(task_list)
+    }
+    return jsonify(output)
+
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+
+    return jsonify({"message": "Não foi possível localizar a atividade"}), 404
 
 
 if __name__ == "__main__":
